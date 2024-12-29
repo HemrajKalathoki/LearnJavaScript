69f0e5e0001924f466c0067d32dc296aee82a56b
HemrajKalathoki <118263381+HemrajKalathoki@users.noreply.github.com>
2024-12-29 15:18:15 +0000

    learned about strings and strings methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Hemraj Kalathoki"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hemraj Kalathoki-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Hemraj Kalathoki    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hemrajkalathoki.com.np"
+
+console.log(url.replace('.com', '-'))
+
+console.log(url.includes('hemraj'))
+
+console.log(gameName.split('-'));
